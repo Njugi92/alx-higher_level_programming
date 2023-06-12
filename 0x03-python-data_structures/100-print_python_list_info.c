069ba4077f7d91b00119c4526a1d557703b890dc
Njugi Karuma <kenkaruma.kk@gmail.com>
2023-06-12 22:38:10 +0000

    100-print_python_list_info

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,8 +9,8 @@ void print_python_list_info(PyObject *p)
 	int size, alloc, j;
 	PyObject *obj;
 
-	size = Py_SIZE(k);
-	alloc = ((PyListObject *)k)->allocated;
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
 
 	printf("[*] Size of the Python List = %d\n", size);
 	printf("[*] Allocated = %d\n", alloc);
@@ -19,7 +19,7 @@ void print_python_list_info(PyObject *p)
 	{
 		printf("Element %d: ", j);
 
-		obj = PyList_GetItem(k, j);
+		obj = PyList_GetItem(p, j);
 		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
