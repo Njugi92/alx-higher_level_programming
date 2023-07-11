d9b03c89b17f2d84c3818294b0a82233f635ffbf
Njugi92 <kenkaruma.kk@gmail.com>
2023-07-11 16:12:30 -0700

    4-from_json_string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""It defines a JSON-to- an object function"""
+
+
+def from_json_string(my_str):
+    """This returns a python object representation of a
+    JSON string
+    """
+    return (json.loads(my_str))
