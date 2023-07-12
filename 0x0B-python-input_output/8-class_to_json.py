9e52a8fad985df6b64b1c73866cc24bd001c5b41
Njugi92 <kenkaruma.kk@gmail.com>
2023-07-11 17:03:48 -0700

    8-class_to_json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This defines a python class to json function"""
+
+
+def class_to_json(obj):
+    """It returns a simple representation of data structure
+    of dictionary
+    """
+    return (obj.__dict__)
