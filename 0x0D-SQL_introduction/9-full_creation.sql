44306dc31a080db0e3bf31bdc753886d352c458e
Njugi92 <kenkaruma.kk@gmail.com>
2023-08-15 16:48:56 -0700

    full table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- To create table and isert various data
+-- To create table and insert various data
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John" 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
