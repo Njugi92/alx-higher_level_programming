e27e102588784d0ddf455d430c5b344377be375e
Njugi92 <kenkaruma.kk@gmail.com>
2023-09-11 12:18:23 -0700

    convert to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-/* script to print mynumber: < first arg convert to integer>
+/* script to print mynumber: 
+ < first arg convert to integer>
  if first arg can be converted to int*/
 
 const argv = parseInt(process.argv[2]);
