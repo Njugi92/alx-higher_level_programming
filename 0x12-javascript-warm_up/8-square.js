382bc4824543721d168bb03780fcf362af363c08
Njugi92 <kenkaruma.kk@gmail.com>
2023-09-11 13:52:01 -0700

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script to print a square
+const size = process.argv[2];
+if (isNaN(size)) {
+	console.log('Missing size');
+}else {
+	for (let k = 0; k < size; k++) {
+		console.log('X'.repeat(size));
+	}
+}
