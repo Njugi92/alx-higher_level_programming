844d6889042e43d8ec5d8fc5315db56a5c101b7a
Njugi92 <kenkaruma.kk@gmail.com>
2023-09-11 13:35:19 -0700

    loop array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* script that prints 3 lines: (like 1-multi_languages.js)
 but by using an array of string and a loop */
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let k = 0; k < myArray.length; k++) {
   console.log(myArray[k]);
 }
