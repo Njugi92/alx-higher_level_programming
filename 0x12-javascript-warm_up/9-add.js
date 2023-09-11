776d3efd3c44490f9590589f271d0eb2152feea1
Njugi92 <kenkaruma.kk@gmail.com>
2023-09-11 14:14:58 -0700

    add integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 // script that prints the addition of 2 integers
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
-function add (a, b) {
+function add(a, b) {
   return a + b;
 }
 console.log(add(a, b))
