068093ec4ffbef0079d4114d54e9dd2a77b82f4e
Njugi92 <kenkaruma.kk@gmail.com>
2023-09-12 14:56:00 -0700

    Dictionary

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Function to import a dictionary of occurrences by user id
+and computes a dictionary of user ids by occurrence */
+const dict = require('./101-data').dict;
+const newDict = {};
+Object.keys(dict).map(function (key, index) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+});
+
+console.log(newDict);
