f2f31a8dbf4b400d13a028c170f02079d5235457
Njugi92 <kenkaruma.kk@gmail.com>
2023-09-15 14:29:57 -0700

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ import MySQLdb
 from sys import argv
 
 # code should not be executed when imported
-if __name__ = '__main__':
+if __name__ == '__main__':
     # Establishing connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
