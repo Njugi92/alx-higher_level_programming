8cc0eddc709571aa138e7c112e323e377082805c
Njugi92 <kenkaruma.kk@gmail.com>
2023-09-15 14:23:04 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """
-The script that takes an argument and displays
-all values in the states table of hbtn_0e_0_usa
-where name matches the argument
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
 """
 import MySQLdb
 from sys import argv
 
-# Code should not be executed when imported
-if __name__ = '__main__':
-    # Establish a connection to the database
+# The code should not be executed when imported
+if __name__ == '__main__':
+
+    # make a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
-    # In order to have multiple separate working environment
-    # via same connection
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
     cur = db.cursor()
-    nameSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
-    cur.execute(nameSr)
+    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
+    cur.execute(nmeSr)
 
     rows = cur.fetchall()
-    for k in rows:
-        print(k)
-    # The clean up process
+    for i in rows:
+        print(i)
+    # Clean up process
     cur.close()
     db.close()
