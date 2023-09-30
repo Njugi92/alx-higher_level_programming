ab188be2041f765ac4bb9bead2e870263ddc97db
Njugi92 <kenkaruma.kk@gmail.com>
2023-09-29 17:03:19 -0700

    catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -o /dev/null -sw "You find me!" 0.0.0.0:5000/catch_me
